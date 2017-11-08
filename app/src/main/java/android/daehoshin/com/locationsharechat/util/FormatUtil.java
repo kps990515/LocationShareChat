0343845838743e65f5aeb6beecc2a4be6722ff6c
KyungSeok <leeseok8347@gmail.com>
2017-11-08 17:07:37 +0900

    권한 수정

---
+++ app/src/main/java/android/daehoshin/com/locationsharechat/util/FormatUtil.java
@@ -0,0 +1,31 @@
+package android.daehoshin.com.locationsharechat.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kyung on 2017-11-08.
+ */
+
+public class FormatUtil {
+    // 비트맵은 닫아야 하는 것 조심
+    public static Bitmap createDrawbleFromView(Context context, View view){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        // view를 bitmap으로 전환
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+}
